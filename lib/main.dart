3bbcc96e5854f91356189743002b6fcc5dd548db
MaxDelissen <maxdelissen17@gmail.com>
2025-03-27 11:14:13 +0100

    Shop page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:obeypay/screens/home_page.dart';
+import 'package:obeypay/screens/shop_page.dart';
 import 'package:obeypay/utils/notificationService.dart';
 
 void main() {
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         // Other default theme properties if we want.
       ),
-      home: const HomePage(),
+      home: const ShopPage(),
     );
   }
 }
