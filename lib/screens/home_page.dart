8cc9969708b423b311740e2d9b3e4e5a06106d00
MaxDelissen <maxdelissen17@gmail.com>
2025-03-28 11:45:21 +0100

    Added tinder swiping cards

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:obeypay/widgets/home_page_widgets/money_slider.dart';
+import 'package:scrumlab_flutter_tindercard/scrumlab_flutter_tindercard.dart';
 
 import '../utils/demo_users.dart' show DemoUsers;
 import '../widgets/home_page_widgets/user_card.dart';
@@ -13,6 +14,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   double sliderValue = 50;
+  final CardController controller = CardController();
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +37,32 @@ class _HomePageState extends State<HomePage> {
                 child: SizedBox(
                   width: 345,
                   height: 480,
-                  child: UserCard(user: DemoUsers[8]),
+                  child: DemoUsers.isNotEmpty
+                      ? TinderSwapCard(
+                    swipeUp: true,
+                    swipeDown: true,
+                    maxWidth: MediaQuery.of(context).size.width * 1.2,
+                    maxHeight: MediaQuery.of(context).size.width * 1.2,
+                    minWidth: MediaQuery.of(context).size.width,
+                    minHeight: MediaQuery.of(context).size.width,
+                    orientation: AmassOrientation.left,
+                    totalNum: DemoUsers.length,
+                    stackNum: 2,
+                    swipeEdge: 4,
+                    cardBuilder: (context, index) => UserCard(user: DemoUsers[index]),
+                    cardController: controller,
+                    swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
+                      if (align.x < 0) {
+                        print("Swiping Left");
+                      } else if (align.x > 0) {
+                        print("Swiping Right");
+                      }
+                    },
+                    swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
+                      print("Card \$index swiped \$orientation");
+                    },
+                  )
+                      : const Center(child: Text("No users available")),
                 ),
               ),
             ),
